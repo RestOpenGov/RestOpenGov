dae063d61ca5e75aa518847168ae7ff1149d1568
opensas <opensas@gmail.com>
2012-12-13 13:05:46 -0300

    agregamos los datos del presupuesto scrappeados

---
+++ visualizaciones/presupuesto/source/scrapper.scala
@@ -0,0 +1,62 @@
+import scala.io.Source.fromFile
+
+object Scrapper {
+
+  def main(args: Array[String]) = {
+    println(args(0))
+    serieToCSV(args(0))
+  }
+
+  def serieToCSV(serie: String): Unit = {
+
+    val lines = fromFile("./" + serie + ".txt").mkString.lines.toList
+
+    val (nums1, texts1) = lines.partition( _.matches("""[0-9\ ,\.]+"""))
+
+    // only keep first num
+    val nums2 = nums1.map ( _.split(" ")(0))
+
+    // drop firt element: Total gasto
+    val (nums, texts) = (nums2.tail, texts1.tail)
+
+    // variable de corte de control
+    var cuenta = ""
+    var rows = List[Row]()
+
+    for (index <- 0 to texts.size-1) {
+      if (isLevel1(texts(index))) {
+        cuenta = texts(index)
+      } else {
+        rows = rows :+ Row(cuenta, texts(index), nums(index))
+      }
+    }
+
+    val out = rows.map(_.toCSV).mkString("\n")
+
+    write("./" + serie + ".csv", out)
+    
+  }
+
+  case class Row(cuenta: String, subcuenta: String, valor: String) {
+    def toCSV: String = {
+      "%s,%s,%s".format(cuenta, subcuenta, valor)
+    }
+  }
+
+  def isNum(value: String): Boolean = {
+    value.matches("""[0-9\ ,\.]+""")
+  }
+
+  def isLevel1(value: String): Boolean = {
+    value.matches("""\d - .*""")
+  }
+
+  def write(fileName: String, content: String, append: Boolean = false):Unit = {
+    import java.io.FileWriter
+
+    val w = new FileWriter(fileName, append)
+    w.write(content)
+    w.close
+  }
+
+}
